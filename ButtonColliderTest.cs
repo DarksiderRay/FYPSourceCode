94250bbe70c991033ff7db3631598a3c35562961
DESKTOP-SPMB6B7\Ray Ng Jun Hui <raika12174@gmail.com>
2018-05-15 14:22:16 +0800

    no message

---
+++ ButtonColliderTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+
+public class ButtonColliderTest : MonoBehaviour {
+
+    public GameObject testObject;
+    public GameObject buttonPressed;
+
+    private static bool canGoLeft;
+    private static bool canGoRight;
+
+    void Start()
+    {
+        canGoLeft = true;
+        canGoRight = true;
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        Debug.Log("Collider: " + collider);
+        if (collider.name == "bone3" || collider.name == "bone2")
+        {
+            if (this.name == "Left Button" && canGoLeft)
+            {
+                this.transform.GetComponent<MeshRenderer>().enabled = false;
+                buttonPressed.GetComponent<MeshRenderer>().enabled = true;
+                testObject.GetComponent<Rigidbody>().velocity = new Vector3(-1f, 0, 0);
+                canGoRight = false;
+            }
+            else if (this.name == "Right Button" && canGoRight)
+            {
+                this.transform.GetComponent<MeshRenderer>().enabled = false;
+                buttonPressed.GetComponent<MeshRenderer>().enabled = true;
+                testObject.GetComponent<Rigidbody>().velocity = new Vector3(1f, 0, 0);
+                canGoLeft = false;
+            }
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        testObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+
+        this.transform.GetComponent<MeshRenderer>().enabled = true;
+        buttonPressed.GetComponent<MeshRenderer>().enabled = false;
+        canGoLeft = true;
+        canGoRight = true;
+    }
+}
---
+++ LeapMotionVRHandle.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Leap.Unity;
+
+public class LeapMotionVRHandle : MonoBehaviour {
+
+    public bool thumb;
+    public bool index;
+    public bool middle;
+    public bool ring;
+    public bool pinky;
+
+    public HandModel leftHandModel;
+    public HandModel rightHandModel;
+
+    private Slider slider;
+    private RectTransform rectTransform;
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
+
+    private float handleMinPositionX;
+    private float handleMaxPositionX;
+
+    void Start()
+    {
+        slider = GetComponentInParent<Slider>();
+        rectTransform = GetComponent<RectTransform>();
+        minX = rectTransform.offsetMin.x;
+        minY = rectTransform.offsetMin.y;
+        maxX = rectTransform.offsetMax.x;
+        maxY = rectTransform.offsetMax.y;
+
+        slider.value = 0;
+        Debug.Log("MinHandlePos: " + transform.localPosition);
+        handleMinPositionX = transform.localPosition.x;
+        slider.value = slider.maxValue;
+        Debug.Log("MaxHandlePos: " + transform.localPosition);
+        handleMaxPositionX = transform.localPosition.x;
+
+        // Default finger boolean values
+        thumb = false;
+        index = true;
+        middle = true;
+        ring = false;
+        pinky = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(leftHandModel.GetPalmNormal());
+
+        //if (leftHandModel.GetPalmNormal().z < 0)
+        //{
+        //    slider.gameObject.SetActive(false);
+        //}
+        //else if (leftHandModel.GetPalmNormal().z > 0)
+        //{
+        //    slider.gameObject.SetActive(true);
+        //}
+
+        // Debug.Log("Handle position: " + this.transform.localPosition + ", minX: " + minX + " , minY: " + minY + " , maxX: " + maxX + " , maxY: " + maxY);
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if ((collider.name == "bone3") && 
+            ((collider.transform.parent.name == "thumb" && thumb) ||
+            (collider.transform.parent.name == "index" && index) ||
+            (collider.transform.parent.name == "middle" && middle) ||
+            (collider.transform.parent.name == "ring" && ring) ||
+            (collider.transform.parent.name == "pinky" && pinky)))
+        {
+            Vector3 colliderRelativeToSliderPosition = slider.transform.InverseTransformPoint(collider.transform.position);
+
+            // Debug.Log(collider.transform.parent.name + " triggered!");
+
+            slider.value = Mathf.Clamp(((colliderRelativeToSliderPosition.x - handleMinPositionX) / (handleMaxPositionX - handleMinPositionX)), 0, slider.maxValue);
+            //Debug.Log("colliderRelativeToSliderPosition: " + colliderRelativeToSliderPosition + ", SliderValue: " + slider.value);
+            //Debug.Log(slider.value);
+        }
+    }
+}
